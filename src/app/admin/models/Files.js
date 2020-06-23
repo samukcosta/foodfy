302844040dcf79c21e3ca6248fbc15d23a88ab17
Samuel Costa <samu.ks@outlook.com>
2020-06-23 12:32:02 -0300

    insertion of images to the bank. sending id information to the recipe_files relationship table

--- src/app/admin/models/Files.js
+++ src/app/admin/models/Files.js
@@ -1,25 +1,32 @@
 const db = require("../../../config/db")
+const Recipe_Files = require("../models/Recipe_Files")
+
 const fs = require("fs")
 
 module.exports = {
-    create(file) {
+    async create({filename, path}, recipeId) {
         const query = `
             INSERT INTO files (
                 name,
                 path
             ) VALUES ($1, $2)
+
             RETURNING id
         `
         const values = [
-            file.filename,
-            file.path
+            filename,
+            path
         ]
 
-        return db.query(query, values)
+        const files = await db.query(query, values)
+
+        Recipe_Files.create(recipeId, files.rows[0].id)
+        
+        return files.rows
     },
     async delete(id){
         try {
-            const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
+            const results = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
             const file = results.rows[0]
 
             fs.unlinkSync(file.path)
