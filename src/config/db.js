0997753db5f28660a4085238bfb5063c514d787e
Samuel Costa <samu.ks@outlook.com>
2020-06-27 02:46:26 -0300

    Pool change for connection via URI

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,5 @@ const dotenv = require('dotenv/config')
 const {Pool} = require("pg")
 
 module.exports = new Pool({
-    host:process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database:process.env.DB_NAME
+    connectionString:process.env.DATABASE_URL
 })
