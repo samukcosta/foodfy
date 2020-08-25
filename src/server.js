8ee69adba7abc19c1c5bef06e849ed6f48b0bfed
Samuel Costa <samu.ks@outlook.com>
2020-08-25 10:59:03 -0300

    user session configuration

--- src/server.js
+++ src/server.js
@@ -3,10 +3,16 @@ const nunjucks = require('nunjucks')
 const routes = require('./routes')
 const cors = require('cors')
 const methodOverride = require('method-override')
+const session = require('./config/session')
 
 const server = express()
 
 server.use(cors())
+server.use(session)
+server.use((req,res,next) => {
+    res.locals.session = req.session
+    next()
+})
 server.use(express.urlencoded({extended: true}))
 server.use(express.static('public'))
 server.use(methodOverride('_method'))
